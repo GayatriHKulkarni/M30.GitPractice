3b79e36f18694b424d81a0b48b60418707903834
hpkul <hpkul@Vedansh>
2025-06-20 07:28:42 +0530

    Commit Demo Class

---
+++ src/test/java/dummy/Demo.java
@@ -0,0 +1,13 @@
+package dummy;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
+
+	}
+
+}
